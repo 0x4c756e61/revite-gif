f7060338a0c72cb79b0df0348335e1aae2c47406
0x454d505459 <40660493+0x454d505459@users.noreply.github.com>
2022-08-06 12:46:24 +0200

    Trying to making it work #2

--- consoleScript.js
+++ consoleScript.js
@@ -2,11 +2,10 @@ var
     msgbox = document.querySelector("#message"),
     body = document.querySelector("body");
 
-let theme = window.state.settings.toJSON()["appearance:theme:overrides"]
+let theme = window.state.settings.toJSON()["appearance:theme:overrides"];
 
-var
-    bg = theme ? theme["background"] : "#242424"
-    accent = theme ? theme["accent"] : "#FD6671"
+let bg = theme ? theme["background"] : "#242424";
+let accent = theme ? theme["accent"] : "#FD6671";
 
 let panelHTML = `
 <div id="panel" style="display: none;position: fixed;z-index: 1;padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;">
@@ -18,7 +17,7 @@ let panelHTML = `
 
     </div>
 </div>
-`
+`;
 
 body.insertAdjacentHTML("afterend", panelHTML);
 
@@ -37,14 +36,14 @@ document.updateUILocation = function() {
 }
 
 document.hide_panel = function() {
-    document.updateUILocation()
+    document.updateUILocation();
 	panel.style.display = 'none';
     gif_zone.innerHTML = "";
 	msgbox.focus();
 }
 
 document.show_panel = function() {
-    document.updateUILocation()
+    document.updateUILocation();
 	panel.style.display = 'block';
 	msgbox.blur();
 
@@ -64,17 +63,17 @@ document.getChannelID = function() {
     var id
     window.state.draft.drafts.toJSON().forEach(arr => {
         if (arr[1] === document.querySelector("#message").value) {
-            id = arr[0]
+            id = arr[0];
         }
     });
-    return id
+    return id;
 }
 
 document.addGif = function(url) {
     console.log(`![gif](${url})`);
     // msgbox.value = `![gif](${url})`;
-    window.state.draft.set(document.getChannelID(), `![gif](${url})`)
-    document.hide_panel()
+    window.state.draft.set(document.getChannelID(), `![gif](${url})`);
+    document.hide_panel();
 }
 
 document.getGifs = function(theUrl, callback)
@@ -98,12 +97,12 @@ document.showGifs = function(responsetext)
 {
     var response_objects = JSON.parse(responsetext);
     let gifs = response_objects["results"];
-    document.updateUILocation()
+    document.updateUILocation();
     gifs.forEach(element => {
-        let url = element["media"][0]["gif"]["url"]
+        let url = element["media"][0]["gif"]["url"];
         // console.log(url)
 
-        gif_zone.insertAdjacentHTML("beforeend", `<img onclick="addGif('${url}')" width=100 style="margin-inline:10px;" src="${url}" alt="tenor gif">`)
+        gif_zone.insertAdjacentHTML("beforeend", `<img onclick="addGif('${url}')" width=100 style="margin-inline:10px;" src="${url}" alt="tenor gif">`);
     });
 
     return;
@@ -114,22 +113,22 @@ document.grab_data = function()
 {
     // set the apikey
     var apikey = "LIVDSRZULELA";
-    var search_url = `https://g.tenor.com/v1/search?q=${msgbox.value}&key=${apikey}&limit=50`
+    var search_url = `https://g.tenor.com/v1/search?q=${msgbox.value}&key=${apikey}&limit=50`;
 
     document.getGifs(search_url,document.showGifs);
-    console.log("grabbed")
+    console.log("grabbed");
     return;
 }
 
 
 window.onkeypress = (e => {
-    document.updateUILocation()
-    panel.remove()
+    document.updateUILocation();
+    panel.remove();
     
     body.insertAdjacentHTML("afterend", panelHTML);
 
     if (e.key === "g" && e.ctrlKey && msgbox.value !== "") {
-        console.log("[Gif-selector] Activated")
+        console.log("[Gif-selector] Activated");
         document.grab_data();
 
         document.show_panel();
